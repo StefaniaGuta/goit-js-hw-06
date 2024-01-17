dcffaeae74f11f148b76bf03637db53a89da1862
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-01-17 07:05:19 -0800

    Task 7

---
+++ src/js/task-07.js
@@ -0,0 +1,8 @@
+const changeInputSize = document.getElementById("font-size-control");
+const textToChange = document.getElementById("text");
+
+changeInputSize.addEventListener('input', e => {
+    textToChange.style.fontSize = changeInputSize.value + 'px';
+});
+
+
