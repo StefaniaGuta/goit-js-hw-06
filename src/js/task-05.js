80bf5cdc9cc46722dd5d69e678fb093ce2228b4e
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-01-19 06:22:40 -0800

    Task 5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,7 +1,6 @@
 const insertName = document.getElementById("name-input");
 const outputSpan = document.getElementById("name-output");
 
-
 insertName.addEventListener('input', e => {
     var inputValue = insertName.value.trim();
     outputSpan.textContent = inputValue || "Anonymous";
